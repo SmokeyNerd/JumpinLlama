c26ce3498427471e9cbc07be145e4060dd18793a
SmokeyLlama <41697893+SmokeyLlama@users.noreply.github.com>
2019-11-13 10:09:14 -0600

    Update jumpin_theme.user.js
    Testing commit webhook.

--- jumpin_theme.user.js
+++ jumpin_theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JumpinLlama : 2019 Theme
 // @namespace    http://www.smokeyllama.com
-// @version      2019.37
+// @version      2019.38
 // @description  Editing Overall Theme of JumpIn. Install and refresh.
 // @author       SmokeyLlama
 // @match        https://jumpin.chat/*

14f20bf9c6231c15b9f8e7bcf2f67f4f7ddac14f
SmokeyLlama <41697893+SmokeyLlama@users.noreply.github.com>
2019-11-19 16:13:09 -0600

    Version 2019.76
    Name Correction

--- jumpin_theme.user.js
+++ jumpin_theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         JumpinLlama : 2019 Theme New
+// @name         JumpinLlama : 2019 Theme
 // @namespace    https://www.smokeyllama.com
-// @version      2019.75
+// @version      2019.76
 // @description  Editing Overall Theme of JumpIn. Install and refresh.
 // @author       SmokeyLlama
 // @match        https://jumpin.chat/*

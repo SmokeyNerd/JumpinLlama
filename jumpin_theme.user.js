0fe4e39c647423c9c93268cbf09d80d4448f2833
Technetium <Technetium1@users.noreply.github.com>
2019-11-19 14:27:20 -0500

    Split vars into multiple statements

--- jumpin_theme.user.js
+++ jumpin_theme.user.js
@@ -3080,7 +3080,10 @@ USER BG
   dragElement(document.getElementById('mydiv'))
 
   function dragElement (elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
+    var pos1 = 0
+    var pos2 = 0
+    var pos3 = 0
+    var pos4 = 0
     if (document.getElementById(elmnt.id + 'header')) {
     // if present, the header is where you move the DIV from:
       document.getElementById(elmnt.id + 'header').onmousedown = dragMouseDown

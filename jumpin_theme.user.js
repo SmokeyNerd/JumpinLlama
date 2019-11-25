ecd04069d3782f2682f1cfc41391dc0879e9044d
SmokeyLlama <41697893+SmokeyLlama@users.noreply.github.com>
2019-11-24 18:31:45 -0600

    For sure fix of dir buttons

--- jumpin_theme.user.js
+++ jumpin_theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JumpinLlama : 2019 Theme
 // @namespace    https://www.smokeyllama.com
-// @version      2019.97
+// @version      2019.98
 // @description  Editing Overall Theme of JumpIn. Install and refresh.
 // @author       SmokeyLlama
 // @match        https://jumpin.chat/*
@@ -22,7 +22,7 @@
  * ------------------------------------------------------------------------
  */
 
- var llama_ver = "04"
+ var llama_ver = "05"
  var file_style = "https://smokeyllama.github.io/JumpinLlama/style.css?ver" + llama_ver
  var file_java = "https://smokeyllama.github.io/JumpinLlama/javascript.js?ver" + llama_ver
 

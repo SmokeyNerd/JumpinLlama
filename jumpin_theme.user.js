8e04aaf61e564a0be94eeab70ea051f9b5f948bf
SmokeyLlama <41697893+SmokeyLlama@users.noreply.github.com>
2019-12-01 16:38:43 -0600

    2019.102

--- jumpin_theme.user.js
+++ jumpin_theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JumpinLlama : 2019 Theme
 // @namespace    https://www.smokeyllama.com
-// @version      2019.101
+// @version      2019.102
 // @description  Editing Overall Theme of JumpIn. Install and refresh.
 // @author       SmokeyLlama
 // @match        https://jumpin.chat/*
@@ -22,7 +22,7 @@
  * ------------------------------------------------------------------------
  */
 
- var llama_ver = "08"
+ var llama_ver = "09"
  var file_style = "https://smokeyllama.github.io/JumpinLlama/style.css?ver" + llama_ver
  var file_java = "https://smokeyllama.github.io/JumpinLlama/javascript.js?ver" + llama_ver
 

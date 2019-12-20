ac12079aabe335fa35e4af6de3165671f7a94a27
SmokeyLlama <41697893+SmokeyLlama@users.noreply.github.com>
2019-12-20 16:11:52 -0600

    Update forcing Css - 2019.103

--- jumpin_theme.user.js
+++ jumpin_theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JumpinLlama : 2019 Theme
 // @namespace    https://www.smokeyllama.com
-// @version      2019.102
+// @version      2019.103
 // @description  Editing Overall Theme of JumpIn. Install and refresh.
 // @author       SmokeyLlama
 // @match        https://jumpin.chat/*
@@ -22,7 +22,7 @@
  * ------------------------------------------------------------------------
  */
 
- var llama_ver = "09"
+ var llama_ver = "10"
  var file_style = "https://smokeyllama.github.io/JumpinLlama/style.css?ver" + llama_ver
  var file_java = "https://smokeyllama.github.io/JumpinLlama/javascript.js?ver" + llama_ver
 
